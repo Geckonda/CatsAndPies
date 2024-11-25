f8b79a9d58b6296737ef4122b5493504fde9dd6a
geckonda <ya.lozhin2014@yandex.ru>
2024-11-25 21:50:26 +0400

    feat(Wallet): add WalletEntity Добавил новую сущность "Кошелек"

---
+++ CatsAndPies.Domain/Entities/WalletEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatsAndPies.Domain.Entities
+{
+    public class WalletEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public UserEntity User { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
