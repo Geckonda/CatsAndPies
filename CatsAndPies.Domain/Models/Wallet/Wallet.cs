e71f6805815fbb8a5a6777017c4221ed76b6ed32
geckonda <ya.lozhin2014@yandex.ru>
2024-11-25 23:03:44 +0400

    feat(Wallet): add Wallet as model Добавил начальную модель Wallet с бизнес логикой

---
+++ CatsAndPies.Domain/Models/Wallet/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatsAndPies.Domain.Models.Wallet
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; private set; }
+
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Сумма должна быть положительной.");
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                throw new InvalidOperationException("Недостаточно средств на кошельке.");
+            Balance -= amount;
+        }
+    }
+}
